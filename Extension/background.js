eab270599e56a27c530f398279e1834099dc86b6
EliKarpinsky <eli@relaymedical.com>
2020-07-07 16:37:49 -0400

    Cleanup

--- Extension/background.js
+++ Extension/background.js
@@ -1,32 +1,16 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function () {
-    const createProperties = {
-        url: "http://www.google.com/search?q=test"
-    }
     chrome.contextMenus.create({
         id: "search",
         title: "Search with Reddit",
         type: 'normal',
-        contexts: ['selection'],
-        // onclick: info => {
-        //
-        //     console.log("test");
-        //     createProperties.url = `http://www.google.com/search?q=${info} reddit`
-        //     // chrome.tabs.create(createProperties);
-        //
-        // }
-
+        contexts: ['selection']
     });
 });
 
 chrome.contextMenus.onClicked.addListener(function (info) {
     if (info.menuItemId === "search") {
-        const createProperties = {
-            url: "http://www.google.com/search?q=test"
-        }
-        console.log(info,"test");
-        createProperties.url = `http://www.google.com/search?q=${info.selectionText} reddit`
-        chrome.tabs.create(createProperties);
+        chrome.tabs.create({url: `http://www.google.com/search?q=${info.selectionText} reddit`});
     }
 });
